cf2a02e7f137cf77abba27b6f4d1172818247f56
Quoc Viet <vietpqfx14832@funix.edu.vn>
2022-09-13 01:56:42 +0700

    update asm1

---
+++ README.md
@@ -0,0 +1,2 @@
+# NJS101x_Assignment1_chinhddFX15068-funix.edu.vn
+First project Node js 
---
+++ routes/other-info.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const otherInfoController = require("../controllers/other-info");
+
+const router = express.Router();
+
+router.get("/staff-info", otherInfoController.getStaffInfo);
+
+router.post("/staff-info", otherInfoController.postStaffInfo) ; 
+
+router.get("/work-info", otherInfoController.getWorkInfo);
+
+router.get("/covid-info", otherInfoController.getCovidInfo);
+
+router.post("/covid-info/temperature", otherInfoController.postBodyTemperature);
+
+router.post("/covid-info/vaccineInfo", otherInfoController.postVaccineInfo);
+
+router.post(
+  "/covid-info/infectCovidInfo",
+  otherInfoController.postInfectCovidInfo
+);
+
+module.exports = router;
